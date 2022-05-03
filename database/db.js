4e94c828c746889b657eaa95a94247208448c0b7
hoangg2022 <102465644+hoangg2022@users.noreply.github.com>
2022-05-03 16:14:18 +0700

    Update db.js

--- database/db.js
+++ database/db.js
@@ -3,9 +3,9 @@ var __path = process.cwd(),
      { color } = require(__path + '/lib/color.js')
 
 // Connection URL
-var url = 'isi dengan url database mongodb';
+var url = 'mongodb+srv://hoang3927:h7team123@bothoang.nojfe.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 try {
-if(url == 'isi dengan url database mongodb') throw console.log(color('Configurasi database, var url belum diisi','red'));
+if(url == 'mongodb+srv://hoang3927:h7team123@bothoang.nojfe.mongodb.net/myFirstDatabase?retryWrites=true&w=majority') throw console.log(color('Configurasi database, var url belum diisi','red'));
 } catch (e) {
 	return;
 	}
